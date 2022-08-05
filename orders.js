7a8690e8bf2077fa099e21a0319ea0fd39cf1690
shultzWilliamLinux <william.shultz2017@gmail.com>
2022-08-05 17:31:58 -0400

    Uploading initial files

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'LAVENDAR Drone',
+        productNumber: '86221',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined'
+    },
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '86531',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Camera Drone',
+        productNumber: '99999',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'GPS 4k Drone',
+        productNumber: '40001',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    }
+]
